be4e49880545e608ca008df556fddb372c64f548
Yash Dave <mail2ypd@gmail.com>
2018-11-10 20:09:54 +0530

    made navbar appear only on login and added about us page

--- checklogstatus.js
+++ checklogstatus.js
@@ -1,6 +1,8 @@
 firebase.auth().onAuthStateChanged(function (user) {
     if (user) {
         document.getElementById('major').hidden = false;
+        document.getElementById('thenav').hidden = false;
+        document.getElementById('loader').hidden = true;
         // User is signed in.
         var displayName = user.displayName;
         var email = user.email;
